08f431b4fe5c23479d6be710310ed36b9e9cde2c
erikasboy <erikas.boy@gmail.com>
2025-03-14 18:39:27 -0400

    Update ESLint configuration to be more permissive and disable ESLint plugin during build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,28 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
+    'react-app',
+    'react-app/jest'
   ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 12,
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
   plugins: ['react'],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'no-debugger': 'warn'
   },
   settings: {
     react: {
